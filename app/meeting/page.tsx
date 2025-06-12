e7fe4d158a4c74e24697f82ac0b7e5dbc3d43b28
virendrakumar1990 <dev.virendraverma@gmail.com>
2025-06-12 19:18:00 +0530

    Add tile layout to fit into screen responsive flex

--- app/meeting/page.tsx
+++ app/meeting/page.tsx
@@ -10,14 +10,11 @@ import TranscriptPanel from "@/components/TranscriptPanel";
 
 export default function MeetingPage() {
   const meetingInfo = useMeetingStore((s) => s.meetingInfo);
-
   const name = meetingInfo.name || "Guest";
-  const camOn = false;   // Default to true for video-only mode
+  const camOn = false; // Default to true for video-only mode
   const lang = meetingInfo.language || "en-US";
   const room = meetingInfo.room;
   const uid = name;
-  
-
 
   const { localTracks, remoteUsers } = useAgora(
     meetingInfo.appId,
@@ -26,40 +23,61 @@ export default function MeetingPage() {
     name,
     camOn
   );
-
+  //const remoteUsers = [...Array(10)]; // testing purpose only
+  const remoteUsersLength = remoteUsers.length;
   return (
     <>
       <div className="flex h-screen bg-gray-900 text-white pr-20">
         {/* Main Screen */}
-        <div className="flex-1 flex p-4 w-full bg-white rounded-xl m-4">
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 w-full transition-all">
-            {localTracks[1] && (
-              <VideoTile
-                user={{
-                  name: String(name),
-                  uid: String(uid),
-                  videoTrack: localTracks[1],
-                }}
-                isLocal={true}
-              />
-            )}
+        <div className="flex-1 flex p-4 w-full bg-white rounded-xl m-4 overflow-auto">
+          <div className="flex flex-wrap justify-center items-center gap-2 w-full relative">
             {remoteUsers.map((user) => (
-              <VideoTile
-                key={String(user.uid)}
-                user={{
-                  name: String(name),
-                  uid: String(user.uid),
-                  videoTrack: user.videoTrack,
-                  hasVideo: user.hasVideo,
-                }}
-                isLocal={false}
-              />
+              <div
+                key={String(user?.uid)}
+                className={`flex justify-center items-center bg-gray-800 rounded-lg overflow-hidden min-w-[100px]
+                     ${
+                       remoteUsersLength === 1
+                         ? "w-full h-full"
+                         : remoteUsersLength === 2
+                         ? "w-1/2 h-1/2"
+                         : remoteUsersLength <= 4
+                         ? "w-1/3 h-1/3"
+                         : remoteUsersLength <= 9
+                         ? "w-1/4 h-1/4"
+                         : "w-1/5 h-1/5"
+                     }`}
+              >
+                <VideoTile
+                  user={{
+                    name: String(name),
+                    uid: String(user?.uid),
+                    videoTrack: user?.videoTrack,
+                    audioTrack: user?.audioTrack,
+                    hasVideo: user?.hasVideo,
+                  }}
+                  isLocal={false}
+                />
+              </div>
             ))}
+            {/* Own Video in Bottom Right Corner */}
+            <div className="absolute bottom-2 right-2 w-40 h-28 bg-black rounded-lg border border-gray-600 shadow-lg">
+              {localTracks[1] && (
+                <VideoTile
+                  user={{
+                    name: String(name),
+                    uid: String(uid),
+                    audioTrack: localTracks[0],
+                    videoTrack: localTracks[1],
+                  }}
+                  isLocal={true}
+                />
+              )}
+            </div>
           </div>
         </div>
 
         {/* Right Sidebar (Chat) */}
-        <ChatPanel uid={uid} room={room}/>
+        <ChatPanel uid={uid} room={room} />
         <TranscriptPanel uid={uid} />
         <ParticipantsPanel
           localUid={uid}
